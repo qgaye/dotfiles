a4d07c5d12d428f9386730fa23c118270481a296
qgaye <qgaye0601@outlook.com>
2020-08-03 14:54:31 +0800

    add nvim

--- script/link.sh
+++ script/link.sh
@@ -1,15 +1,19 @@
 rm ~/.zshrc
-ln -s ~/Library/Mobile Documents/com~apple~CloudDocs/Dotfiles/.zshrc ~/.zshrc
+ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Dotfiles/.zshrc ~/.zshrc
 
 rm ~/.vimrc
-ln -s ~/Library/Mobile Documents/com~apple~CloudDocs/Dotfiles/.vimrc ~/.vimrc
+ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Dotfiles/.vimrc ~/.vimrc
+
+rm ~/.config/nvim/init.vim
+mkdir ~/.config/nvim
+ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Dotfiles/init.vim ~/.config/nvim/init.vim
 
 rm ~/.gitconfig
-ln -s ~/Library/Mobile Documents/com~apple~CloudDocs/Dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Dotfiles/.gitconfig ~/.gitconfig
 
 rm ~/.ssh/config
-ln -s ~/Library/Mobile Documents/com~apple~CloudDocs/Dotfiles/.ssh/config ~/.ssh/config
+ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Dotfiles/.ssh/config ~/.ssh/config
 
 rm ~/.myclirc
-ln -s ~/Library/Mobile Documents/com~apple~CloudDocs/Dotfiles/.myclirc ~/.myclirc
+ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Dotfiles/.myclirc ~/.myclirc
 
